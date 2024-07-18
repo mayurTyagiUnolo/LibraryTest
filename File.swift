7cddee19bb9b182d2239e4ab2f4b4059dae688e8
Mayur <mayurtyagi@unolo.com>
2024-07-18 18:45:29 +0530

    class created

---
+++ File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  LibraryTest
+//
+//  Created by Mayur  on 18/07/24.
+//
+
+import Foundation
+
+public class Hello{
+    public init(){}
+    public func(){
+        print("Hello world")
+    }
+    
+}
